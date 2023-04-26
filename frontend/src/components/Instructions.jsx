dceff7ad018bcb599ecc2c1cbb53d82c0db1a87d
Anjali Gupta <anjaligupta0621@gmail.com>
2023-04-26 14:40:49 -0400

    instructions complete

--- frontend/src/components/Instructions.jsx
+++ frontend/src/components/Instructions.jsx
@@ -97,31 +97,6 @@ function Instructions(props) {
                         "Hello, Stop listening" <br />
                         You can also press 'Stop' button.
                         </Typography>
-import React from 'react';
-import {useEffect, useState} from 'react';
-import { Box, Stack, Typography, Grid } from '@mui/material';
-import { Sidebar } from '.';
-
-function Instructions(props) {
-    const [selectedCategory, setSelectedCategory] = useState("Instructions");
-
-    return (            
-        <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-            <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-                <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
-                <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-                    Copyright © 2023 UFL
-                </Typography>
-            </Box>
-            <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-                <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-                    {selectedCategory}
-                </Typography>
-            </Box>
-        </Stack>
-    )
-}
-                        
 
                     </Typography>
                     <Typography variant="h6" ml={4} sx={{ color: "white" }}>
@@ -134,5 +109,4 @@ function Instructions(props) {
     )
 }
 
-export default Instructions;
 export default Instructions;
